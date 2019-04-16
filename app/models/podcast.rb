9ad6af0023cbd379b359202e03760f27f05cdfed
github username <github email address>
2019-04-16 17:11:57 +0100

    Added all views for Podcasts

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -2,4 +2,6 @@ class Podcast < ApplicationRecord
   has_many :episodes
   has_many :categorisations
   has_many :categories, through: :categorisations
+  has_many :subscriptions
+  has_many :users, through: :subscriptions
 end
