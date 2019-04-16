b0cdae317824cd820102e3e5ef89ff1d9e3f1656
github username <github email address>
2019-04-16 15:39:05 +0100

    updated fave to fafe

--- app/models/favouritisation.rb
+++ app/models/favouritisation.rb
@@ -1,4 +1,4 @@
 class Favouritisation < ApplicationRecord
-  belongs_to :fave, class_name: "Episode", foreign_key: "episode_id"
+  belongs_to :fafe, class_name: "Episode", foreign_key: "episode_id"
   belongs_to :user
 end
