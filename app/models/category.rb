01e626e687807066db58702a5937733886b01b80
github username <github email address>
2019-04-17 13:28:01 +0100

    fixed category selection on main page without button

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,9 @@
 class Category < ApplicationRecord
   has_many :categorisations
   has_many :podcasts, through: :categorisations
+
+  def options_display
+    "#{self.name} (#{self.podcasts.count})"
+  end
+
 end
