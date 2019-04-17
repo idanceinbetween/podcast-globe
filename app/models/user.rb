495ecf39c6877fa802a56feca4095ed1a380109b
github username <github email address>
2019-04-17 14:11:46 +0100

    cosmetic update on model

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,7 @@ class User < ApplicationRecord
 
   has_many :active_relationships, class_name:  "Followship", foreign_key: "follower_id", dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
-  has_many :passive_relationships, class_name:  "Followship",
-                               foreign_key: "followed_id", #myself in this column, so I could get my array of many followers.
-                               dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Followship", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :follower
 
 end
